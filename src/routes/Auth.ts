11c795beba4f135eb3c494250a224cf80989afbb
filipexxborba <borba.filipe00@gmail.com>
2022-08-15 15:05:11 -0300

    Commit - Allow lowercase in username

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -8,29 +8,32 @@ import { UserModel } from "../models/User";
 // Login Route
 router.post("/", (req: Request, res: Response) => {
   const { username, password } = req.body;
-  UserModel.findOne({ username: username }, (error: Error, user: any) => {
-    if (error) res.status(500).send(error);
-    if (!user)
-      res
-        .status(200)
-        .send(
-          JSON.stringify({ status: false, message: "Usuário não encontrado" })
+  UserModel.findOne(
+    { username: username.toLowerCase() },
+    (error: Error, user: any) => {
+      if (error) res.status(500).send(error);
+      if (!user)
+        res
+          .status(200)
+          .send(
+            JSON.stringify({ status: false, message: "Usuário não encontrado" })
+          );
+      if (user && user.password !== password)
+        res.status(200).send(
+          JSON.stringify({
+            status: false,
+            message: "Senha incorreta, verifique novamente.",
+          })
         );
-    if (user && user.password !== password)
-      res.status(200).send(
-        JSON.stringify({
-          status: false,
-          message: "Senha incorreta, verifique novamente.",
-        })
-      );
-    if (user && user.password === password) {
-      user.last_login = new Date();
-      user
-        .save()
-        .then((user) => res.status(200).send(user))
-        .catch((error) => res.status(500).send(error));
+      if (user && user.password === password) {
+        user.last_login = new Date();
+        user
+          .save()
+          .then((user) => res.status(200).send(user))
+          .catch((error) => res.status(500).send(error));
+      }
     }
-  });
+  );
 });
 
 // Create User Route
@@ -39,7 +42,7 @@ router.post("/create", (req: Request, res: Response) => {
   if (username && password && role) {
     const user = new UserModel({
       _id: new mongoose.Types.ObjectId(),
-      username: username,
+      username: username.toLowerCase(),
       password: password,
       role: role,
     });
